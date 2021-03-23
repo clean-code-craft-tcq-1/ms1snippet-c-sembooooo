f8fcf855ef08c130a835686f07bd86a5017a1c4c
Sana Srikar (RBEI/ESP-IS14) <Srikar.Sana@in.bosch.com>
2021-03-23 19:13:41 +0530

    updating the testcases

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -9,8 +9,35 @@ TEST_CASE("reports error when soc jumps abruptly") {
   REQUIRE(validateSOCreadings(socReadings, numOfSocReadings) == 0);
 }
 
+TEST_CASE("reports sucess when a smooth transition present in soc ") {
+  double socReadings[] = {0.0, 0.01, 0.02, 0.03};
+  int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
+  REQUIRE(validateSOCreadings(socReadings, numOfSocReadings) == 1);
+}
+
+TEST_CASE("reports sucess when a smooth transition present in soc ") {
+  double socReadings[] = {0.0, 0.01, 0.02, 0.03};
+  int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
+  REQUIRE(validateSOCreadings(socReadings, numOfSocReadings) == 1);
+}
+
+
 TEST_CASE("reports error when current jumps abruptly") {
   double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
   REQUIRE(validateSOCreadings(currentReadings, numOfCurReadings) == 0);
+}
+
+
+TEST_CASE("reports sucess when a smooth transition present in current") {
+  double currentReadings[] = {0.0, 0.1, 0.2, 0.3};
+  int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
+  REQUIRE(validateSOCreadings(currentReadings, numOfCurReadings) == 1);
+}
+
+TEST_CASE("Checks if assertion is failed during null pointer as parameter ") {
+  double *Readings = 0;
+  int numOfReadings = 10 /*dummy value */
+  double maxDelta = 5.0;
+  REQUIRE_THROWS(IsChangeinValuesAbrupt(Readings,numOfReadings,maxDelta));
 }
