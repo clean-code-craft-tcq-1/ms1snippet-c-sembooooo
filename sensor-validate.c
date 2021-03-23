7b4606ed477b47071a95069ef485ca27560b8c87
Sana Srikar (RBEI/ESP-IS14) <Srikar.Sana@in.bosch.com>
2021-03-23 18:21:15 +0530

    Found some context based name

--- sensor-validate.c
+++ sensor-validate.c
@@ -1,30 +1,47 @@
 #include "sensor-validate.h"
 
-int IsDifferenceNotMoreThanMaxDelta(double value, double nextValue, double maxDelta) {
+#define TRUE    1
+#define FALSE   0
+
+int IsDifferenceMoreThanMaxDelta(double value, double nextValue, double maxDelta) {
   if(nextValue - value > maxDelta) {
-    return 0;
+    return 1;
   }
-  return 1;
+  return 0;
 }
 
-int IsListConsecutiveValuesDiffNotMoreThanMaxDelta (double* values, int numOfValues, double maxDelta)
+
+int IsChangeinValuesAbrupt (double* values, int numOfValues, double maxDelta)
 {
   int lastButOneIndex = numOfValues - 1;
+  assert(values != 0);
   for(int i = 0; i < lastButOneIndex; i++) {
-    if(IsDifferenceNotMoreThanMaxDelta(values[i], values[i + 1],maxDelta) == 0) {
-      return 0;
+    if(IsDifferenceMoreThanMaxDelta(values[i], values[i + 1],maxDelta) == FALSE) {
+      return 1;
     }
   }
   return 0;
 }
 
 
-int validateSOCreadings(double* values, int numOfValues) {
-  double SOCReadings_maxDelta = 0.05 ;
-  return IsListConsecutiveValuesDiffNotMoreThanMaxDelta(values,numOfValues, SOCReadings_maxDelta); 
+int validateSOCreadings(double* values, int numOfValues)
+{
+  double maxDeltabetweenReadings = 0.05 ;
+
+  if(IsChangeinValuesAbrupt(values,numOfValues,maxDeltabetweenReadings) == TRUE)
+  {
+    return 0;
+  }
+  return 1;  
 }
 
-int validateCurrentreadings(double* values, int numOfValues) {
-  double CurrentReadings_maxDelta = 0.1 ;
-  return IsListConsecutiveValuesDiffNotMoreThanMaxDelta(values,numOfValues, CurrentReadings_maxDelta); 
+int validateCurrentreadings(double* values, int numOfValues)
+{
+  double maxDeltabetweenReadings = 0.1 ;
+
+  if(IsChangeinValuesAbrupt(values,numOfValues,maxDeltabetweenReadings) == TRUE)
+  {
+    return 0;
+  }
+  return 1; 
 }
