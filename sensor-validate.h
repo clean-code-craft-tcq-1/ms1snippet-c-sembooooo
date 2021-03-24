de6c5282b1fc9a9b0287c5661a590daa62f8b9a7
Sana Srikar (RBEI/ESP-IS14) <Srikar.Sana@in.bosch.com>
2021-03-24 18:51:24 +0530

    correcting .h file

--- sensor-validate.h
+++ sensor-validate.h
@@ -3,6 +3,6 @@ int IsDifferenceMoreThanMaxDelta(double value, double nextValue, double maxDelta
 
 int IsChangeinValuesAbrupt (double* values, int numOfValues, double maxDelta);
 
-int validateSOCreadings(double* values, int numOfValues); 
+int IsChangeInSOCReadingsAbrupt(double* values, int numOfValues); 
 
-int validateCurrentreadings(double* values, int numOfValues);
+int IsChangeInCurrentReadingsAbrupt(double* values, int numOfValues);
