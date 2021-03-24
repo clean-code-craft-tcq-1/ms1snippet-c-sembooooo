256f7623364164a2d990ed2ca559d7fdefea0801
Sana Srikar (RBEI/ESP-IS14) <Srikar.Sana@in.bosch.com>
2021-03-24 09:23:51 +0530

    reverting back the changes

--- sensor-validate.c
+++ sensor-validate.c
@@ -11,7 +11,7 @@ int IsDifferenceMoreThanMaxDelta(double value, double nextValue, double maxDelta
 }
 
 
-inline int IsChangeinValuesAbrupt (double* values, int numOfValues, double maxDelta)
+int IsChangeinValuesAbrupt (double* values, int numOfValues, double maxDelta)
 {
   int lastButOneIndex = numOfValues - 1;
   for(int i = 0; i < lastButOneIndex; i++) {
