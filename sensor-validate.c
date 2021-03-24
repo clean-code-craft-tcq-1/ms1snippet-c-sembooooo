b80c057d6222bb7498d22918940394024d033778
Sana Srikar (RBEI/ESP-IS14) <Srikar.Sana@in.bosch.com>
2021-03-24 17:58:51 +0530

    renamed  maxDeltabetweenReadings to  maxDeltabetweenValues

--- sensor-validate.c
+++ sensor-validate.c
@@ -5,9 +5,9 @@
 
 int IsDifferenceMoreThanMaxDelta(double value, double nextValue, double maxDelta) {
   if(nextValue - value > maxDelta) {
-    return 1;
+    return TRUE;
   }
-  return 0;
+  return FALSE;
 }
 
 
@@ -16,18 +16,18 @@ int IsChangeinValuesAbrupt (double* values, int numOfValues, double maxDelta)
   int lastButOneIndex = numOfValues - 1;
   for(int i = 0; i < lastButOneIndex; i++) {
     if(IsDifferenceMoreThanMaxDelta(values[i], values[i + 1],maxDelta) == TRUE) {
-      return 1;
+      return TRUE;
     }
   }
-  return 0;
+  return FALSE;
 }
 
 
 int validateSOCreadings(double* values, int numOfValues)
 {
   int IsValidationPassed = TRUE;
-  double maxDeltabetweenReadings = 0.05 ;
-  if(IsChangeinValuesAbrupt(values,numOfValues,maxDeltabetweenReadings) == TRUE)
+  double maxDeltabetweenValues = 0.05 ;
+  if(IsChangeinValuesAbrupt(values,numOfValues,maxDeltabetweenValues) == TRUE)
   {
     IsValidationPassed = FALSE;
   }
@@ -37,9 +37,9 @@ int validateSOCreadings(double* values, int numOfValues)
 int validateCurrentreadings(double* values, int numOfValues)
 {
   int IsValidationPassed = TRUE;
-  double maxDeltabetweenReadings = 0.1 ;
+  double maxDeltabetweenValues = 0.1 ;
 
-  if(IsChangeinValuesAbrupt(values,numOfValues,maxDeltabetweenReadings) == TRUE)
+  if(IsChangeinValuesAbrupt(values,numOfValues,maxDeltabetweenValues) == TRUE)
   {
     IsValidationPassed = FALSE;
   }
