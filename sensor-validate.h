601ee3ed02e504fef09e2bca436eca13c48c39c1
Sana Srikar (RBEI/ESP-IS14) <Srikar.Sana@in.bosch.com>
2021-03-24 16:14:35 +0530

    to restart builds

--- sensor-validate.h
+++ sensor-validate.h
@@ -3,6 +3,6 @@ int IsDifferenceMoreThanMaxDelta(double value, double nextValue, double maxDelta
 
 int IsChangeinValuesAbrupt (double* values, int numOfValues, double maxDelta);
 
-int validateSOCreadings(double* values, int numOfValues);
+int validateSOCreadings(double* values, int numOfValues); 
 
 int validateCurrentreadings(double* values, int numOfValues);
